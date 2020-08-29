dbc82ce041ad47c2297cec685b6e1b3b90717ca8
Lior Dolev <liordolev7@gmail.com>
2020-08-30 00:38:59 +0300

    home page css

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -9,13 +9,50 @@ const Home = () => {
     <Page className={'home'}>
       <div className={'image-container'}>
         <img src={cactus} />
-        <Button inverted>משכך כאבים EP זמין להאזנה</Button>
+        <Button
+          as={'a'}
+          href={'https://orcd.co/lior_dolevep'}
+          target={'_blank'}
+          inverted
+        >
+          משכך כאבים EP זמין להאזנה
+        </Button>
         <nav>
-          <Icon name={'instagram'} />
-          <Icon name={'facebook f'} />
-          <Icon name={'spotify'} />
-          <Icon name={'apple'} />
-          <Icon name={'youtube'} />
+          <a
+            className={'link'}
+            href={'https://www.instagram.com/liordolev'}
+            target="_blank"
+          >
+            <Icon name={'instagram'} />
+          </a>
+          <a
+            className={'link'}
+            href={'https://www.facebook.com/liordolevofficial'}
+            target="_blank"
+          >
+            <Icon name={'facebook f'} />
+          </a>
+          <a
+            className={'link'}
+            href={'https://spoti.fi/3bwNfX8'}
+            target="_blank"
+          >
+            <Icon name={'spotify'} />
+          </a>
+          <a
+            className={'link'}
+            href={'https://apple.co/2yyAtZJ'}
+            target="_blank"
+          >
+            <Icon name={'apple'} />
+          </a>
+          <a
+            className={'link'}
+            href={'https://www.youtube.com/user/liordolev'}
+            target="_blank"
+          >
+            <Icon name={'youtube'} />
+          </a>
         </nav>
       </div>
       <Letter />
