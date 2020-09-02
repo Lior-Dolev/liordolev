768ecddf5b4420de21328c29dc7c7ef56d0e60de
Lior Dolev <liordolev7@gmail.com>
2020-09-02 16:48:23 +0300

    tour

--- src/containers/Tour/Tour.tsx
+++ src/containers/Tour/Tour.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Page } from '@layouts';
+import Text from './Text';
+import TourImg from './Tour.jpg';
 
-const Tour = () => <Page>הופעות</Page>;
+const Tour = () => (
+  <Page className={'tour'}>
+    <div className={'image'}>
+      <img src={TourImg} />
+    </div>
+    <Text />
+  </Page>
+);
 
 export default Tour;
